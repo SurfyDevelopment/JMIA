d85e12247acf11bba6c362e3727b5b1ce308978b
Surfy <colnaghiandrea@outlook.it>
2023-09-17 14:20:35 +0200

    Added license

--- src/main/java/it/surfy/jmia/api/mcita/endpoints/Server.java
+++ src/main/java/it/surfy/jmia/api/mcita/endpoints/Server.java
@@ -1,7 +1,33 @@
+/*
+ * Copyright (c) 2023 https://minecraft-italia.net/ | Surfy
+ *
+ * Permission is hereby granted, free of charge, to any person
+ * obtaining a copy of this software and associated documentation
+ * files (the "Software"), to deal in the Software without
+ * restriction, including without limitation the rights to use,
+ * copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following
+ * conditions:
+ *
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+ * OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+ * HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+ * WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+ * OTHER DEALINGS IN THE SOFTWARE.
+ */
 package it.surfy.jmia.api.mcita.endpoints;
 
 import com.google.gson.annotations.SerializedName;
-import it.surfy.jmia.api.mcita.endpoints.type.*;
+import it.surfy.jmia.api.mcita.endpoints.type.Link;
+import it.surfy.jmia.api.mcita.endpoints.type.ServerVersions;
+import it.surfy.jmia.api.mcita.endpoints.type.Tag;
 import lombok.Getter;
 
 import java.util.List;
